6da053f3fc98322d64a39d5bb9aa4a005779b33b
Tabassum Hassan <tiram819@gmail.com>
2020-03-14 18:51:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,8 +132,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     alans_new_info = "GUI"
-      programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
   programmer_hash
+
 
 end
